063e3652acbfefb85ccefb863c385fa8e54e98cb
Carlos Palomino <palominocp@outlook.com>
2020-10-21 17:25:00 -0500

    Level 1 reviewed

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -2,32 +2,30 @@
 var myMap = L.map("map", {
     center: [37.09, -95.71],
     zoom: 5
-  });
+});
   
-  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
     id: "mapbox/streets-v11",
     accessToken: API_KEY
-  }).addTo(myMap);
+}).addTo(myMap);
   
-  // Define a markerSize function that will give each city a different radius based on its population
-  function markerSize(population) {
-    return population / 40;
-  }
-  
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
-    /*
-    locations = [];
-    for (var i = 0; i < data.features.length; i++) {
-        locations.push([data.features[i].geometry.coordinates[1], data.features[i].geometry.coordinates[0]]);
-    }
-    */
-    for (var i = 0; i < data.features.length; i++) {
+function getColor(d) {
+    return d === 1 ? '#B7F34D':
+           d === 2 ? '#E1F34D':
+           d === 3 ? '#F3DB4D':
+           d === 4 ? '#F3BA4D':
+           d === 5 ? '#F0A76B':
+                     '#F06B6B';
+};
 
-        // Conditionals for countries points
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+    for (var i = 0; i < data.features.length; i++) {
+        // Conditionals for magnitude colors
         var color = "";
         var magnitude = data.features[i].properties.mag
         if (magnitude <= 1) {
@@ -48,10 +46,10 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         else {
             color = "rgb(240,107,107";
         }
-        lat = data.features[i].geometry.coordinates[1];
-        lng = data.features[i].geometry.coordinates[0];
+    
+        var lat = data.features[i].geometry.coordinates[1];
+        var lng = data.features[i].geometry.coordinates[0];
         L.circle([lat,lng], {
-            //fillOpacity: data.features[i].properties.mag / 10,
             fillOpacity: 1,
             color: "black",
             weight: 0.5,
@@ -60,56 +58,23 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
         }).bindPopup("<h2>" + "Location: " + data.features[i].properties.place + "</h2> <hr> <h3>Magnitude: " + data.features[0].properties.mag + "</h3>").addTo(myMap);
     }
 
-     // Set up the legend
-    var legend = L.control({ position: "bottomright" });
+    // Set up the legend
+    var legend = L.control({position: "bottomright" });
     legend.onAdd = function() {
         var div = L.DomUtil.create("div", "info legend");
-        //var limits = geojson.options.limits;
-        //var colors = geojson.options.colors;
-        grades = [0, 1, 2, 3, 4, 5];
-        //var labels = [];
+        var grades = [0, 1, 2, 3, 4, 5];
+        var labels = [];
+        var from;
+        var to;
 
-        // Add min & max
         for (var i = 0; i < grades.length; i++) {
-            div.innerHTML += "<i style='background:' + getColor(grades[i] + 1)></i>";
+            from = grades [i];
+            to = grades [i+1];
+            labels.push("<div class=\"item\"><li style=\"background-color: " + getColor(from + 1) + "\"></li>" + "<span>" + from + (to ? "&ndash;"+ to : "+") + "</span>" + "</div>");
         }
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
         return div;
     };
+    legend.addTo(myMap);
 
-    // div.innerHTML = legendInfo;
-    /*
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-    */
-/*
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-    */
-
-
-  // Adding legend to the map
-  legend.addTo(myMap);
-
-
-
-
-    console.log(data.features[0].properties.mag);
-    console.log(data.features[0].geometry.coordinates[1]);
-    console.log(data.features[0].geometry.coordinates[0]);
-    //console.log(locations);
-    console.log(data.features.length);
-    // Loop through the cities array and create one marker for each city object
-    /*
-    for (var i = 0; i < cities.length; i++) {
-        L.circle(cities[i].location, {
-            fillOpacity: 0.75,
-            color: "white",
-            fillColor: "purple",
-            // Setting our circle's radius equal to the output of our markerSize function
-            // This will make our marker's size proportionate to its population
-            radius: markerSize(cities[i].population)
-        }).bindPopup("<h2>" + "Location: " + data.features[i].properties.place + "</h2> <hr> <h3>Magnitude: " + data.features[0].properties.mag + "</h3>").addTo(myMap);
-    }
-    */
 });
